416e3f8667b8a9718a9b65e64f5b0f75a4a338e2
matt <mathewandnana@gmail.com>
2023-03-17 21:27:32 -0500

    setting up local host....adding server.js.....getting intial webpage loaded

---
+++ server.js
@@ -0,0 +1,28 @@
+// Require dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Set up Express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware for parsing JSON data
+app.use(express.json());
+
+// Serve static files from public directory
+app.use(express.static('public'));
+
+// HTML routes
+app.get('\Develop\public\notes.html', (req, res) => {
+  res.sendFile(path.join(__dirname,'Develop', 'public', 'notes.html')); 
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Develop','public', 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
