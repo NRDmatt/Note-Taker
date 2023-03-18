0120130f92f19a36ebb4c9dfd34807c865681691
matt <mathewandnana@gmail.com>
2023-03-17 21:53:20 -0500

    getting get started button to send the user to the second page

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // HTML routes
-app.get('\Develop\public\notes.html', (req, res) => {
-  res.sendFile(path.join(__dirname,'Develop', 'public', 'notes.html')); 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'Develop', 'public',  'notes.html')); 
 });
 
 app.get('*', (req, res) => {
