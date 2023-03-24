a07fbfbfc82fb86ae449cbeac0ba8fa17fad7ec9
matt <mathewandnana@gmail.com>
2023-03-23 19:07:08 -0500

    changing server js to run through the api/notes correctly

--- server.js
+++ server.js
@@ -1,78 +1,59 @@
-// Require dependencies
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
-// html routes
-// /notes will now send the user to notes.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname,'public', 'notes.html')); 
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-
-// intial webpage to be loaded will be index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'public', 'index.html'));
-});
-
-app.get("/api/notes", (req, res) => {
-  // Read the db.json file
-  const dbFilePath = path.join(__dirname, "db", "db.json");
-  fs.readFile(dbFilePath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Failed to retrieve notes." });
-    }
-
-    // Parse the JSON data and send it as the response
+app.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, 'db', 'db.json'), (err, data) => {
+    if (err) throw err;
     const notes = JSON.parse(data);
     res.json(notes);
   });
 });
 
-app.post("/api/notes", (req, res) => {
-  // Read the db.json file
-  const dbFilePath = path.join(__dirname, "db", "db.json");
-  fs.readFile(dbFilePath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Failed to retrieve notes." });
-    }
-
-    // Parse the JSON data and get the notes array
+app.post('/api/notes', (req, res) => {
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text
+  };
+  fs.readFile(path.join(__dirname, 'db', 'db.json'), (err, data) => {
+    if (err) throw err;
     const notes = JSON.parse(data);
-
-    // Create a new note object with a unique id
-    const newNote = {
-      id: uuidv4(),
-      title: req.body.title,
-      text: req.body.text,
-    };
-
-    // Add the new note to the notes array
     notes.push(newNote);
-
-    // Write the updated notes array back to db.json
-    fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Failed to save note." });
-      }
-
-      // Send the new note as the response
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
+      if (err) throw err;
       res.json(newNote);
     });
   });
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  fs.readFile(path.join(__dirname, 'db', 'db.json'), (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const filteredNotes = notes.filter(note => note.id !== id);
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(filteredNotes), (err) => {
+      if (err) throw err;
+      res.sendStatus(204);
+    });
+  });
 });
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT} http://localhost:3000`));
